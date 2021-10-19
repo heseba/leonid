c242170d386c12619516df30631c1f241982baf4
Sebastian Heil <sebastian.heil@informatik.tu-chemnitz.de>
2021-10-19 17:44:40 +0200

    initial commit

---
+++ ann.py
@@ -0,0 +1,92 @@
+# https://colab.research.google.com/drive/1xK4-uFEIbS0czVjTHEAgWG2MYuz_jwrr?usp=sharing
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Dense
+from tensorflow import keras
+import pandas as pd
+from tensorflow.keras import backend as K
+from kerastuner.tuners import RandomSearch, Hyperband, BayesianOptimization
+
+
+def maximum_absolute_scaling(df):
+    df_scaled = df.copy()
+    for column in df_scaled.columns:
+        df_scaled[column] = df_scaled[column] / df_scaled[column].abs().max()
+    return df_scaled
+
+
+def load_data(path):
+    data = pd.read_csv(path, delimiter=',')
+    y = data.loc[:, 'Orderliness']
+    print(y)
+    x = data.loc[:, 'VA_PNG':'grid quality']
+    print(x)
+    x = maximum_absolute_scaling(x)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=0)
+    return x_train, y_train, x_test, y_test
+
+
+x_train, y_train, x_test, y_test = load_data("train_without_index.csv")
+
+print(x_train, y_train, x_test, y_test)
+
+def coeff_determination(y_true, y_pred):
+    SS_res =  K.sum(K.square( y_true-y_pred ))
+    SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
+    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+
+def build_model(hp):
+    model = keras.Sequential()
+    activation_choice = hp.Choice('activation', values=['relu', 'sigmoid', 'tanh', 'elu', 'selu'])
+    model.add(Dense(units=hp.Int('units_input',
+                                 min_value=512,
+                                 max_value=1024,
+                                 step=32),
+                    activation=activation_choice))
+    for i in range(2, 6):
+        model.add(Dense(units=hp.Int('units_' + str(i),
+                                     min_value=128,
+                                     max_value=1024,
+                                     step=32),
+                        activation=activation_choice))
+    model.add(Dense(1))
+    model.compile(
+        optimizer="adam",
+        loss='mse',
+        metrics=['mse', coeff_determination])
+    return model
+
+
+tuner = RandomSearch(
+    build_model,
+    objective='mse',
+    max_trials=20,
+    directory='ASN_AST',
+    overwrite=True
+)
+
+tuner.search_space_summary()
+
+tuner.search(x_train, y_train,
+             epochs=100,
+             validation_split=0.2,callbacks=[keras.callbacks.TensorBoard('history14'), keras.callbacks.EarlyStopping(monitor='val_loss',
+                              min_delta=0,
+                              patience=6,
+                              verbose=0, mode='auto')])
+
+tuner.results_summary()
+
+print(tuner.oracle.get_best_trials()[0].trial_id)
+
+models = tuner.get_best_models(num_models=1)
+
+for model in models:
+    model.evaluate(x_test, y_test)
+    model.summary()
+
+
+    # for i in range(1,100):
+    #     res = model.predict(np.expand_dims(x_test.iloc[[i]], axis=0))
+    #     print(x_test.iloc[[i]])
+    #     print(res)
+    #     print(np.argmax(res))
---
+++ cnn.py
@@ -0,0 +1,110 @@
+# https://colab.research.google.com/drive/1FOxO55_MmyJDoorQ329xabaxKZ03Rv0z?usp=sharing
+
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import tensorflow
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import GlobalAveragePooling2D, AveragePooling2D, Input,Concatenate, Dense, Flatten, Dropout, Conv2D, MaxPooling2D, Convolution2D, BatchNormalization
+import cv2
+
+def get_class(name):
+    data = pd.read_csv("integer.csv", delimiter=',')
+    rslt_df = data.loc[data['filename'] == name]['Aesthetics'].values[0]
+    print(rslt_df)
+    return rslt_df
+
+
+def load_data(data_dir):
+    directories = [d for d in os.listdir(data_dir)
+                   if os.path.isdir(os.path.join(data_dir, d))]
+    labels = []
+    images = []
+    for d in directories:
+        label_dir = os.path.join(data_dir, d)
+        file_names = [os.path.join(label_dir, f)
+                      for f in os.listdir(label_dir)
+                      if f.endswith(".png")]
+        index = 0
+        for f in file_names:
+            img = cv2.imread(f)
+            try:
+                imresize = cv2.resize(img, (900, 600))
+                name_temp = get_class(f.replace("images2\screens\\", ""))
+                if name_temp.size != 0:
+                    images.append(imresize)
+                    index += 1
+                    labels.append(name_temp)
+
+            except Exception as e:
+                print(str(e))
+
+    return images, labels
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.compat.v1.Session(config=config)
+
+X_train, y_train = load_data("images2")
+X_train = np.array(X_train).astype('float32')
+X_train = X_train / 255.0
+
+print(y_train)
+y_train = np.array(y_train)
+print(y_train)
+
+def coeff_determination(y_true, y_pred):
+    SS_res =  K.sum(K.square( y_true-y_pred ))
+    SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
+    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+
+def createCNNModel():
+    def Inception_block(input_layer, f1, f2_conv1, f2_conv3, f3_conv1, f3_conv5, f4):
+        path1 = Conv2D(filters=f1, kernel_size=(1, 1), padding='same', activation='relu')(input_layer)
+        path2 = Conv2D(filters=f2_conv1, kernel_size=(1, 1), padding='same', activation='relu')(input_layer)
+        path2 = Conv2D(filters=f2_conv3, kernel_size=(3, 3), padding='same', activation='relu')(path2)
+        path3 = Conv2D(filters=f3_conv1, kernel_size=(1, 1), padding='same', activation='relu')(input_layer)
+        path3 = Conv2D(filters=f3_conv5, kernel_size=(5, 5), padding='same', activation='relu')(path3)
+        path4 = MaxPooling2D((3, 3), strides=(1, 1), padding='same')(input_layer)
+        path4 = Conv2D(filters=f4, kernel_size=(1, 1), padding='same', activation='relu')(path4)
+
+        output_layer = tensorflow.keras.layers.concatenate([path1, path2, path3, path4], axis=-1)
+
+        return output_layer
+
+
+    input_layer = Input(shape=(600, 900, 3))
+
+    X = Conv2D(filters=64, kernel_size=(7, 7), strides=2, padding='valid', activation='relu')(input_layer)
+    X = MaxPooling2D(pool_size=(3, 3), strides=2)(X)
+    X = Conv2D(filters=64, kernel_size=(1, 1), strides=1, padding='same', activation='relu')(X)
+    X = Conv2D(filters=192, kernel_size=(3, 3), padding='same', activation='relu')(X)
+    X = MaxPooling2D(pool_size=(3, 3), strides=2)(X)
+    X = Inception_block(X, f1=64, f2_conv1=96, f2_conv3=128, f3_conv1=16, f3_conv5=32, f4=32)
+    X = Inception_block(X, f1=128, f2_conv1=128, f2_conv3=192, f3_conv1=32, f3_conv5=96, f4=64)
+    X = MaxPooling2D(pool_size=(3, 3), strides=2)(X)
+    X = Inception_block(X, f1=192, f2_conv1=96, f2_conv3=208, f3_conv1=16, f3_conv5=48, f4=64)
+    X = Inception_block(X, f1=160, f2_conv1=112, f2_conv3=224, f3_conv1=24, f3_conv5=64, f4=64)
+    X = Inception_block(X, f1=128, f2_conv1=128, f2_conv3=256, f3_conv1=24, f3_conv5=64, f4=64)
+    X = Inception_block(X, f1=112, f2_conv1=144, f2_conv3=288, f3_conv1=32, f3_conv5=64, f4=64)
+    X = GlobalAveragePooling2D(name='GAPL')(X)
+    X = Dropout(0.4)(X)
+    X = Dense(1)(X)
+
+    model = Model(input_layer, X)
+
+    epochs = 40
+    model.compile(loss='mse', optimizer='adam', metrics=['mae', 'mse', coeff_determination])
+    return model, epochs
+
+model, epochs = createCNNModel()
+
+history = model.fit(X_train, y_train, validation_split=0.20, epochs=epochs, callbacks=[keras.callbacks.TensorBoard('history11'), keras.callbacks.EarlyStopping(monitor='val_loss',
+                              min_delta=0,
+                              patience=6,
+                              verbose=0, mode='auto')])
