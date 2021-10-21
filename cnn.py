f8cff09ede1bc0d01b862ee361c1c93bdb668364
Sebastian Heil <sebastian.heil@informatik.tu-chemnitz.de>
2021-10-21 15:55:01 +0200

    refactoring: extracted parameters, removed unnecessary clutter, renamings to better reflect the content of variables

--- cnn.py
+++ cnn.py
@@ -12,60 +12,52 @@ import numpy as np
 from tensorflow import keras
 from tensorflow.keras import backend as K
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import GlobalAveragePooling2D, AveragePooling2D, Input,Concatenate, Dense, Flatten, Dropout, Conv2D, MaxPooling2D, Convolution2D, BatchNormalization
+from tensorflow.keras.layers import GlobalAveragePooling2D, AveragePooling2D, Input, Concatenate, Dense, Flatten, \
+    Dropout, Conv2D, MaxPooling2D, Convolution2D, BatchNormalization
 import cv2
 
-def get_class(name):
-    data = pd.read_csv("integer.csv", delimiter=',')
-    rslt_df = data.loc[data['filename'] == name]['Aesthetics'].values[0]
-    print(rslt_df)
-    return rslt_df
+
+EPOCHS = 40
+RESIZE_X = 900
+RESIZE_Y = 600
+TARGET_METRIC = 'Aesthetics'
+
+
+def value_of_metric(screenshot_name, all_metrics):
+    metric_value = all_metrics.loc[all_metrics['filename'] == screenshot_name][TARGET_METRIC].values[0]
+    print(metric_value)
+    return metric_value
 
 
 def load_data(data_dir):
-    data_dir = Path(data_dir)
-    directories = [d for d in data_dir.iterdir() if d.is_dir()]
-    labels = []
+    all_metrics = pd.read_csv("integer.csv", delimiter=',')
+
     images = []
-    for d in directories:
-        screenshots = d.glob('*.png')
+    metric_values = []
 
-        index = 0
-        for screenshot in screenshots:
-            img = cv2.imread(str(screenshot.resolve()))
-            try:
-                print('1')
-                imresize = cv2.resize(img, (900, 600))
-                print('2')
-                name_temp = get_class(screenshot.name)
-                print('3')
-                if name_temp.size != 0:
-                    print('4')
-                    images.append(imresize)
-                    index += 1
-                    labels.append(name_temp)
+    screenshots = Path(data_dir).glob('*/*.png')
 
-            except Exception as e:
-                print(str(e))
+    for screenshot in screenshots:
+        img = cv2.imread(str(screenshot.resolve()))
+        try:
+            imresize = cv2.resize(img, (RESIZE_X, RESIZE_Y))
+            metric_value = value_of_metric(screenshot.name, all_metrics)
+            if metric_value.size != 0:
+                images.append(imresize)
+                metric_values.append(metric_value)
 
-    return images, labels
+        except Exception as e:
+            print('EXCEPTION ' + str(e))
+            print(screenshot.name)
 
-config = tf.compat.v1.ConfigProto()
-config.gpu_options.allow_growth = True
-session = tf.compat.v1.Session(config=config)
+    return images, metric_values
 
-X_train, y_train = load_data("images2")
-X_train = np.array(X_train).astype('float32')
-X_train = X_train / 255.0
-
-print(y_train)
-y_train = np.array(y_train)
-print(y_train)
 
 def coeff_determination(y_true, y_pred):
-    SS_res =  K.sum(K.square( y_true-y_pred ))
-    SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
-    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+    SS_res = K.sum(K.square(y_true - y_pred))
+    SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
+    return (1 - SS_res / (SS_tot + K.epsilon()))
+
 
 def createCNNModel():
     def Inception_block(input_layer, f1, f2_conv1, f2_conv3, f3_conv1, f3_conv5, f4):
@@ -81,8 +73,7 @@ def createCNNModel():
 
         return output_layer
 
-
-    input_layer = Input(shape=(600, 900, 3))
+    input_layer = Input(shape=(RESIZE_Y, RESIZE_X, 3))
 
     X = Conv2D(filters=64, kernel_size=(7, 7), strides=2, padding='valid', activation='relu')(input_layer)
     X = MaxPooling2D(pool_size=(3, 3), strides=2)(X)
@@ -102,13 +93,31 @@ def createCNNModel():
 
     model = Model(input_layer, X)
 
-    epochs = 40
     model.compile(loss='mse', optimizer='adam', metrics=['mae', 'mse', coeff_determination])
-    return model, epochs
+    return model
 
-model, epochs = createCNNModel()
 
-history = model.fit(X_train, y_train, validation_split=0.20, epochs=epochs, callbacks=[keras.callbacks.TensorBoard('history11'), keras.callbacks.EarlyStopping(monitor='val_loss',
-                              min_delta=0,
-                              patience=6,
-                              verbose=0, mode='auto')])
+if __name__ == '__main__':
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = tf.compat.v1.Session(config=config)
+
+    X_train, y_train = load_data("images2")
+    X_train = np.array(X_train).astype('float32')
+    X_train = X_train / 255.0
+
+    print(y_train)
+    y_train = np.array(y_train)
+    print(y_train)
+    model = createCNNModel()
+
+
+    print(f'\n\nTraining Model for {TARGET_METRIC}\n\n')
+
+    history = model.fit(X_train, y_train, validation_split=0.20, epochs=EPOCHS,
+                        callbacks=[keras.callbacks.TensorBoard('history11'),
+                                   keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                                 min_delta=0,
+                                                                 patience=6,
+                                                                 verbose=0, mode='auto')])
+    print(f'\n\nTraining Done')
